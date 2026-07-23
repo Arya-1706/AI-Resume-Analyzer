cacfc993ed865c2c3fc29c337a1e370af49d41f8
Arya Jha <aryajha697@gmail.com>
2026-07-23 16:20:17 +0530

    Updated frontend of Interview plan

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ const interviewRouter = require("./routes/interview.routes")
 
 /* using all the routes here */
 app.use("/api/auth",authRouter)
-app.use("api/interview", interviewRouter)
+app.use("/api/interview", interviewRouter)
 
 
 module.exports = app
