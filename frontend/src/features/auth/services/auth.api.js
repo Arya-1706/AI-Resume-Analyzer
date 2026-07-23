ee135bde21532b88e4b55bbc60d4437f976df551
Arya Jha <aryajha697@gmail.com>
2026-07-23 17:47:12 +0530

    Use CLIENT_URL for CORS

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 
 })
@@ -65,3 +65,5 @@ export async function getMe() {
 
   }
 }
+
+export default api
